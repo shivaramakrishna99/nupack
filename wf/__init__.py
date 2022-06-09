12ddf8e384be9299a6698641e24ce6f6bcc1dd57
Shivaramakrishna Srinivasan <shivaramakrishna.srinivasan@gmail.com>
2022-06-09 18:53:14 +0530

    Added README and About section

--- wf/__init__.py
+++ wf/__init__.py
@@ -14,7 +14,6 @@ from latch import small_task, workflow
 from latch.types import LatchFile
 from natsort import as_ascii
 
-
 from nupack import *  # Import NUPACK
 
 class Material(Enum):
@@ -86,10 +85,23 @@ def nupack_loop_stack(
     # NUPACK - Loop Free Energy and Stacking State Energies
     ---
 
-    ## About
+    ## **About**
     ---
-    NUPACK is a growing software suite for the analysis and design of nucleic acid structures, devices, and systems serving the needs of researchers in the fields of nucleic acid nanotechnology, molecular programming, synthetic biology, and across the life sciences more broadly.
+    [NUPACK](https://docs.nupack.org/#about) is a growing software suite for the analysis and design of nucleic acid structures, devices, and systems serving the needs of researchers in the fields of nucleic acid nanotechnology, molecular programming, synthetic biology, and across the life sciences more broadly.
 
+    ## **Citations**
+    ---
+    ### NUPACK Analysis Algorithms
+    **Complex analysis and test tube analysis**
+	- M.E. Fornace, N.J. Porubsky, and N.A. Pierce (2020). A unified dynamic programming framework for the analysis of interacting nucleic acid strands: enhanced models, scalability, and speed.  [ACS Synth Biol](https://pubs.acs.org/doi/abs/10.1021/acssynbio.9b00523) , 9:2665-2678, 2020. ( [pdf](http://www.nupack.org/downloads/serve_public_file/fornace20.pdf?type=pdf) ,  [supp info](http://www.nupack.org/downloads/serve_public_file/fornace20_supp.pdf?type=pdf) )
+	- R. M. Dirks, J. S. Bois, J. M. Schaeffer, E. Winfree, and N. A. Pierce. Thermodynamic analysis of interacting nucleic acid strands.  [SIAM Rev](http://epubs.siam.org/doi/abs/10.1137/060651100) , 49:65-88, 2007. ( [pdf](http://www.nupack.org/downloads/serve_public_file/sirev07.pdf?type=pdf) )
+    **Pseudoknot analysis**
+	- R. M. Dirks and N. A. Pierce. An algorithm for computing nucleic acid base-pairing probabilities including pseudoknots.  [J Comput Chem](http://onlinelibrary.wiley.com/doi/10.1002/jcc.10296/abstract) , 25:1295-1304, 2004. ( [pdf](http://www.nupack.org/downloads/serve_public_file/jcc04.pdf?type=pdf) )
+	- R. M. Dirks and N. A. Pierce. A partition function algorithm for nucleic acid secondary structure including pseudoknots.  [J Comput Chem](http://onlinelibrary.wiley.com/doi/10.1002/jcc.20057/abstract) , 24:1664-1677, 2003. ( [pdf](http://www.nupack.org/downloads/serve_public_file/jcc03.pdf?type=pdf) ,  [supp info](http://www.nupack.org/downloads/serve_public_file/jcc03_supp.pdf?type=pdf) )
+
+    **Workflow Repository** - https://github.com/shivaramakrishna99/nupack/
+    **Acknowledgements** - https://docs.nupack.org/#acknowledgments
+    **License** - https://docs.nupack.org/#license
     
 
 When finishing a project that has benefited from NUPACK calculations, please remember to cite the NUPACK web application and algorithms appropriately; citations are an important component in helping to secure funding for NUPACK development and maintenance. Please email us with questions, comments, feature requests, and bug reports at support@nupack.org.
@@ -98,7 +110,7 @@ When finishing a project that has benefited from NUPACK calculations, please rem
         display_name: NUPACK - Loop Free Energy and Stacking State Energies
         
         author:
-            name: NUPACK
+            name: The NUPACK Team
             email: support@nupack.org
             github: https://github.com/beliveau-lab/NUPACK
         
